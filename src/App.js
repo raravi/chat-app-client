ca1a53159a3f15de57e0730dd872823c3476ae49
Amith Raravi <amith.raravi@gmail.com>
2020-02-07 23:29:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { subscribeToTimer, subscribeToNewMessages, sendMessage } from './api';
 import './App.css';
 
 function App() {
+  let [ timestamp, setTimestamp ] = useState(moment());
+  let [ messageBoxText, setMessageBoxText ] = useState([]);
+  let [ newMessage, setNewMessage ] = useState('');
+
+  useEffect(() => {
+    subscribeToTimer(3000, (timestamp) => setTimestamp(timestamp));
+    subscribeToNewMessages( (message) => setNewMessage(message));
+  }, []);
+
+  useEffect(() => {
+    newMessageReceived(newMessage);
+  }, [newMessage]);
+
+  function getInput(e) {
+    var input = document.getElementsByClassName("message")[0];
+    if (e.key === "Enter" && input.value !== "") {
+      let tempMsges = messageBoxText.slice();
+      sendMessage(input.value);
+      tempMsges.push(input.value);
+      setMessageBoxText(tempMsges);
+      input.value = "";
+
+    }
+  }
+
+  function getInputMessage() {
+    var input = document.getElementsByClassName("message")[0];
+    if (input.value !== "") {
+      let tempMsges = messageBoxText.slice();
+      sendMessage(input.value);
+      tempMsges.push(input.value);
+      setMessageBoxText(tempMsges);
+      input.value = "";
+    }
+  }
+
+  function newMessageReceived(message) {
+    if(message !== '') {
+      let tempMsges = messageBoxText.slice();
+      tempMsges.push(message);
+      setMessageBoxText(tempMsges);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Hello World!</p>
+      <p>This is the timer value: {moment(timestamp).format('h:mm:ss a, DD/MM/YYYY')}</p>
+      <textarea name="messagebox" className="messagebox" readOnly value={messageBoxText}/>
+      <br />
+      <input type="text" name="message" className="message" placeholder="Type here..." onKeyUp={(e) => getInput(e)} />
+      <button type="button" onClick={getInputMessage}>Submit</button>
+    </>
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,17 @@
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://192.168.1.99:8000');
+
+function subscribeToTimer(interval, cb) {
+  socket.on('timer', timestamp => cb(timestamp));
+  socket.emit('subscribeToTimer', interval);
+}
+
+function subscribeToNewMessages(cb) {
+  socket.on('newMessage', message => cb(message));
+}
+
+function sendMessage(message) {
+  socket.emit('sendMessage', message);
+}
+
+export { subscribeToTimer, subscribeToNewMessages, sendMessage };
